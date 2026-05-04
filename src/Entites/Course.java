d22d54e1fb98af8a21cc7a425551df1ad1ff02d7
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-04 10:16:14 +0400

    Create Course class

---
+++ src/Entites/Course.java
@@ -0,0 +1,44 @@
+package Entites;
+
+public class Course {
+
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    private String courseCode;
+    private String lecturer;
+    private String time;
+    private String day;
+
+
+}
