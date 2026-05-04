09622bc24d80f38531b5798931f9240482cb881a
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-04 10:50:13 +0400

    create Menu Message

---
+++ src/Utils/MenuMessages.java
@@ -0,0 +1,25 @@
+package Utils;
+
+public class MenuMessages {
+    public static String MAIN_MENU_MESSAGE = """
+            1- Department Menu
+            2- Student Menu
+            3- Exit
+            """;
+    public static String DEPARTMENT_MENU_MESSAGE = """
+            1- Department Add
+            2- Department Update
+            3- Department Delete
+            4- Department Display by Name
+            5- Department Display All
+            6- Go back to Main Menu
+            """;
+    public static String STUDENT_MENU_MESSAGE = """
+            1- Register Courses
+            2- Drop Course
+            3- View Schedule
+            4- Student Display by Name
+            5- Student Display All
+            6- Go back to Main Menu
+            """;
+}
