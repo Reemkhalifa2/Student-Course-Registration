e02a49cea8e51f8b1f0fd0b57ffd20b9f2998404
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-05-04 10:26:36 +0400

    declare adddepartment method

--- src/Behaviour/DepartmentInterface.java
+++ src/Behaviour/DepartmentInterface.java
@@ -1,8 +1,10 @@
 package Behaviour;
 
 import Entites.Course;
+import Entites.Department;
 
 public interface DepartmentInterface {
+    Boolean addDepartment(Department department);
     Boolean addCourse(Course course);
     Boolean removeCourse(Course course);
     Boolean modifyCourse(String courseCode, Course updatedCourse);
